e1196685ac6d816babe97c008014d4b24575df7a
MaheshB77 <mbansode7696@gmail.com>
2023-02-22 18:55:08 +0530

    Added docker-compose and refactored some code

--- visits/README.md
+++ visits/README.md
@@ -1,6 +1,9 @@
 # Visits application
+    - This application shows number of user visited the server
+    - This application will use redis server to store the number of visits
+    - This means that we have two servers communicating in this application
+    - In such scenarios we can use docker-compose
 
-- This application shows number of user visited the server
-- This application will use redis server to store the number of visits
-- This means that we have two servers communicating in this application
-- In such scenarios we can use docker-compose
+## How to run this application
+    - Execute the below command to run the application
+        - docker-compose up
--- visits/app.js
+++ visits/app.js
@@ -2,21 +2,23 @@ const express =  require("express");
 const redis =  require("redis");
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+    url: "redis://redis-server:6379",
+});
 
 client.connect().then(() => {
+    console.log("redis connected successfully");
     client.set("visits", 0);
 }).catch((error) => {
     console.error("Error while connecting to redis server :: " + error);
 });
 
-app.get("/", (req, res) => {
-    client.get("visits", (err, visits) => {
-        res.send("Number of visits is :: " + visits);
-        client.set("visits", parseInt(visits) + 1);
-    })
+app.get("/", async (req, res) => {
+    const visits = await client.get("visits");
+    await client.set("visits", parseInt(visits) + 1)
+    res.send("Number of visits is :: " + visits);
 });
 
-app.listen(8081, () => {
-    console.log("Application started on port 8081");
+app.listen(8085, () => {
+    console.log("Application started on port 8085");
 });
