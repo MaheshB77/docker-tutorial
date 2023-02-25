233c0ee89cd714f024471d993df3e706b3351856
MaheshB77 <mbansode7696@gmail.com>
2023-02-26 02:22:31 +0530

    Added redis config and express routes

--- complex/server/index.js
+++ complex/server/index.js
@@ -17,11 +17,50 @@ const pgClient = new Pool({
     password: keys.pgPassword,
     port: keys.pgPort
 });
-
 pgClient.on('error', () =>
     console.error('Error while connecting to postgres db')
 );
-
 pgClient
     .query('CREATE TABLE IF NOT EXISTS values (number INT)')
     .catch((error) => console.log('Error while creating the table'));
+
+// Redis setup
+const redis = require('redis');
+const redisClient = redis.createCluster({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
+
+// Express routes handlers
+app.get('/', (req, res) => {
+    res.send('Hi');
+});
+
+app.get('/values/all', async (req, res) => {
+    const values = await pgClient.query('SELECT * FROM values');
+    res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+    const values = await redisClient.hGetAll('values');
+    res.send(values);
+});
+
+app.post('/values', async (req, res) => {
+    const index = req.body.index;
+    if (parseInt(index) > 40) {
+        return res.status(422).send('Index too high');
+    }
+
+    redisClient.hSet('values', index, 'Nothing yet!');
+    redisPublisher.publish('insert', index);
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+    res.send({ working: true });
+});
+
+app.listen(5000, (error) => {
+    console.log("Listening");
+});
