204b3be638d709813574242370c1fb55c205786b
MaheshB77 <mbansode7696@gmail.com>
2023-03-02 02:28:39 +0530

    Added some elements in Fib component

--- complex/client/src/Fib.js
+++ complex/client/src/Fib.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export default function Fib() {
     const [seenIndexes, setSeenIndexes] = useState([]);
     const [values, setValues] = useState({});
-    const [index, setIndex] = useState('');
+    const [index, setIndex] = useState({});
 
     useEffect(() => {
         fetchIndexes();
@@ -21,5 +21,45 @@ export default function Fib() {
         setSeenIndexes(seenIndexes.data);
     };
 
-    return <div>Fib</div>;
+    const renderSeenIndexes = () => {
+        return seenIndexes.map(({ number }) => number).join(', ');
+    };
+
+    const renderValues = () => {
+        const entries = [];
+        for (const key in values) {
+            entries.push(
+                <div key={key}>
+                    For index {key} I calculated {values[key]}
+                </div>
+            );
+        }
+        return entries;
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        await axios.post('/api/values', {
+            index
+        });
+        setIndex({ index: '' });
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>Enter your index : </label>
+                <input
+                    value={index}
+                    onChange={(evt) => setIndex({ index: evt.target.value })}
+                />
+                <button>Submit</button>
+            </form>
+            <h3>Indexes I have seen : </h3>
+            {renderSeenIndexes()}
+
+            <h3>Calculated values : </h3>
+            {renderValues()}
+        </div>
+    );
 }
